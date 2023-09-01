cf6f5a9c16e54963c644d83d3259adba6f597bc0
Alvin Lee <alvin.lee@aquaticinformatics.com>
2023-09-01 15:12:12 -0700

    delete versioned object test

--- S3Playground/Program.cs
+++ S3Playground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Amazon.S3;
 using Amazon.S3.Model;
@@ -23,16 +24,44 @@ namespace S3Playground
 
       var s3Client = new AmazonS3Client();
 
+      Console.WriteLine($"Getting list of object versions for {_options.ObjectKey}");
       var listResponse = await GetObjectVersions(s3Client);
       foreach(S3ObjectVersion v in listResponse.Versions)
       {
         Console.WriteLine($"{v.Key}-{v.VersionId}");
+      }
+
+      Console.WriteLine($"Deleting object {_options.ObjectKey} from s3");
+      var deleteResponse = await DeleteAttachment(s3Client, listResponse);
+      foreach (DeletedObject deletedObject in deleteResponse.DeletedObjects)
+      {
+        Console.WriteLine($"Deleted: {deletedObject.Key}-{deletedObject.VersionId}-{deletedObject.DeleteMarker}");
+      }
+
+      if (deleteResponse.DeleteErrors.Any())
+      {
+        foreach (var error in deleteResponse.DeleteErrors)
+        {
+          Console.WriteLine($"Error deleting {error.Key}-{error.VersionId}: {error.Message}");
+        }
       }
     }
 
     private static async Task<ListVersionsResponse> GetObjectVersions(IAmazonS3 s3Client)
     {
       return await s3Client.ListVersionsAsync(_options.BucketName, _options.ObjectKey);
+    }
+
+    private static async Task<DeleteObjectsResponse> DeleteAttachment(IAmazonS3 s3Client,
+      ListVersionsResponse versionList)
+    {
+      var deleteRequest = new DeleteObjectsRequest
+      {
+        BucketName = _options.BucketName,
+        Objects = versionList.Versions.Select(v => new KeyVersion { Key = v.Key, VersionId = v.VersionId }).ToList()
+      };
+
+      return await s3Client.DeleteObjectsAsync(deleteRequest);
     }
   }
 }
